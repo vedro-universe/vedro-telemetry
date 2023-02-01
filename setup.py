c4f63a563e0ee76f5e39b67bbaa19132a5e42cf7
Nikita Tsvetkov <tsv1@fastmail.com>
2023-02-01 19:54:23 +0400

    bump version → 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro-telemetry",
-    version="0.1.0",
+    version="0.2.0",
     description="Vedro plugin for self-hosted telemetry",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
